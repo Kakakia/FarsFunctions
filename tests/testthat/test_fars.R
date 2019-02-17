9d040a0d1b93b746a7c2461a066a4cd6b63378b4
Ankai Xu <ankai.xu@gmail.com>
2019-02-17 14:58:36 +0100

    test fix

--- tests/testthat/test_fars.R
+++ tests/testthat/test_fars.R
@@ -1,6 +1,9 @@
+library(testthat)
+f<-file(system.file("R", "fars_functions.R", package = "farsfunctions"), open = "r")
+source(f)
+close(f)
+
 expect_equal(make_filename(2013), "accident_2013.csv.bz2")
 expect_equal(dim(fars_summarize_years(c(2013:2015))), c(12, 4))
 expect_equal(length(fars_read_years(c(2013:2015))), 3)
 
-#Must print without error.
-fars_map_state(33, 2013)
